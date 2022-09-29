945398390943a8462b99eef71fc30f4d9589a174
Colin Duggan <duggan.colin@gmail.com>
2022-09-29 22:25:01 +0100

    Handle string and number literals

--- src/main/java/com/cdugga/Scanner.java
+++ src/main/java/com/cdugga/Scanner.java
@@ -14,13 +14,13 @@ public class Scanner {
   private int line = 1;
 
 
-  Scanner(String source){
+  Scanner(String source) {
     this.source = source;
   }
 
-  List<Token> scanTokens(){
+  List<Token> scanTokens() {
 
-    while(!isAtEnd()){
+    while (!isAtEnd()) {
       // we are at the beginning of the next lexeme
       start = current;
       scanToken();
@@ -29,72 +29,171 @@ public class Scanner {
     return tokens;
   }
 
-  private boolean isAtEnd(){
+  private boolean isAtEnd() {
     return current >= source.length();
   }
 
-  private void scanToken(){
+  private void scanToken() {
     char c = advance();
-    switch(c){
-      case '(': addToken(LEFT_PAREN); break;
-      case ')': addToken(RIGHT_PAREN); break;
-      case '{': addToken(LEFT_BRACE); break;
-      case '}': addToken(RIGHT_BRACE); break;
-      case ',': addToken(COMMA); break;
-      case '.': addToken(DOT); break;
-      case '-': addToken(MINUS); break;
-      case '+': addToken(PLUS); break;
-      case ';': addToken(SEMICOLON); break;
-      case '*': addToken(STAR); break;
-      case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
-      case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
-      case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
-      case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
-      case ' ': ;
+    switch (c) {
+      case '(':
+        addToken(LEFT_PAREN);
+        break;
+      case ')':
+        addToken(RIGHT_PAREN);
+        break;
+      case '{':
+        addToken(LEFT_BRACE);
+        break;
+      case '}':
+        addToken(RIGHT_BRACE);
+        break;
+      case ',':
+        addToken(COMMA);
+        break;
+      case '.':
+        addToken(DOT);
+        break;
+      case '-':
+        addToken(MINUS);
+        break;
+      case '+':
+        addToken(PLUS);
+        break;
+      case ';':
+        addToken(SEMICOLON);
+        break;
+      case '*':
+        addToken(STAR);
+        break;
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+      case ' ':
+        ;
       case '\r':
       case '\t':
         break;
       case '\n':
-        line++ ; break;
+        line++;
+        break;
       case '/':
-        if(match('/')){
+        if (match('/')) {
           // a comment goes until the end of the line
-          while(peek() != '\n' && !isAtEnd())
+          while (peek() != '\n' && !isAtEnd()) {
             advance();
-          } else {
-            addToken(SLASH);
           }
+        } else {
+          addToken(SLASH);
+        }
+      case '"':
+        string();
+        break;
       default:
-        App.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        }
+//        else if ( isAlpha(c) ){
+//          identifier();
+//        }
+        else {
+          App.error(line, "Unexpected character.");
+        }
         break;
     }
   }
 
-  private char advance(){
+  private char advance() {
     current++;
     return source.charAt(current - 1);
   }
 
-  private void addToken(TokenType type){
+  private void addToken(TokenType type) {
     addToken(type, null);
   }
 
-  private void addToken(TokenType type, Object literal){
+  private void addToken(TokenType type, Object literal) {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
   }
 
-  private boolean match(char expected){
-    if(isAtEnd()) return false;
-    if(source.charAt(current) != expected) return false;
+  private boolean match(char expected) {
+    if (isAtEnd()) {
+      return false;
+    }
+    if (source.charAt(current) != expected) {
+      return false;
+    }
 
     current++;
     return true;
   }
 
-  private char peek(){
-    if(isAtEnd()) return '\0';
+  private char peek() {
+    if (isAtEnd()) {
+      return '\0';
+    }
     return source.charAt(current);
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') {
+        line++;
+      }
+      advance();
+    }
+
+    // unterminated string
+    if (isAtEnd()) {
+      App.error(line, "Unterminated string.");
+      return;
+    }
+
+    // the closing "
+    advance();
+
+    // trim the surrounding quotes
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private void number() {
+    while (isDigit(peek())) {
+      advance();
+    }
+
+    // look for a fractional part
+    if (peek() == '.' && isDigit(peekNext())) {
+      // consume the "."
+      advance();
+
+      while (isDigit(peek())) {
+        advance();
+      }
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) {
+      return '\0';
+    }
+    return source.charAt(current + 1);
   }
 
 }
